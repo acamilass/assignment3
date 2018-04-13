196a0050c5a369ec58a9105b16d1aad52285006f
VagnerSilva <vagner23silva@gmail.com>
2018-04-13 14:03:00 -0300

    layout login

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,24 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule, MatCardModule, MatMenuModule, MatToolbarModule, MatIconModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatRadioModule,
+  MatButtonToggleModule
+} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
-    MatButtonModule, 
-    MatCardModule, 
-    MatMenuModule, 
-    MatToolbarModule, 
-    MatIconModule
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonToggleModule
   ],
   declarations: [],
 
   exports: [
-    MatButtonModule, 
-    MatCardModule, 
-    MatMenuModule, 
-    MatToolbarModule, 
-    MatIconModule
+    MatButtonModule,
+    MatCardModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatRadioModule,
+    MatButtonToggleModule
   ]
 
 })
