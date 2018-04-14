419ce337779be02b2f616b4cba2ed7a27567f64c
Camila Santos <acamilass@gmail.com>
2018-04-13 21:38:19 -0300

    criando layout quest

--- src/app/quest/quest.module.ts
+++ src/app/quest/quest.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { QuestRoutingModule } from './quest-routing.module';
 import { QuestComponent } from './quest.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    QuestRoutingModule
+    QuestRoutingModule,
+    SharedModule 
   ],
   declarations: [QuestComponent]
 })
