8062e15fbf74d17c4dd2f3ba5a21c8b6936e6718
Camila Santos <acamilass@gmail.com>
2018-04-15 20:17:42 -0300

    atualizando instruções

--- src/app/instrucoes/instrucoes.module.ts
+++ src/app/instrucoes/instrucoes.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { InstrucoesRoutingModule } from './instrucoes-routing.module';
 import { InstrucoesComponent } from './instrucoes.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    InstrucoesRoutingModule
+    InstrucoesRoutingModule,
+    SharedModule 
   ],
   declarations: [InstrucoesComponent]
 })
