c27971ed237026c749814cd9cd4ae9d24e342c67
Camila Santos <acamilass@gmail.com>
2018-04-16 22:56:53 -0300

    add status login

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class AuthService {
 
-  public authState: any=null;
-
-  constructor() { }
+  public authState: any = null;    
+  
+  constructor(private af: AngularFireAuth) {     
+    this.af.authState.subscribe(auth => {       
+    this.authState = auth;     });   
+  }
 
   // Returns true if user is logged in
   get authenticated(): boolean {
